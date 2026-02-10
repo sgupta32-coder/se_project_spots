367e834a7a1b89584374c0e378219bfb412b0d84
webhead <guptasarthak908@gmail.com>
2026-02-10 17:02:26 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initalCards = [
+const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
@@ -92,7 +92,7 @@ function handleAddCardSubmit(evt) {
 
 addCardFormElemet.addEventListener("submit", handleAddCardSubmit);
 
-initalCards.forEach(function (item) {
+initialCards.forEach(function (item) {
   console.log(item.name);
   console.log(item.link);
 });
